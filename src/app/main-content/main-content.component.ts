7f02e266b1fb01a949e645c7e7d39d104f313cfc
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-29 09:21:13 +0200

    aboutme

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

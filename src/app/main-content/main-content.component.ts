2638191bd6ff8442bf165248ae063a27c1f85749
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-29 13:48:10 +0200

    home/curve fixed

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomeComponent, AboutMeComponent],
+  imports: [HomeComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

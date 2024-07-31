53beaeeebf84035dca7b989abab73b0c0c96a9cf
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-31 12:59:07 +0200

    contact/beginn

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
+  imports: [HomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

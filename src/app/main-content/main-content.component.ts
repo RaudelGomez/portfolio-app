219ed674ee92bfd3624ae46c1b7fd6d7c0c03864
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-06 15:22:41 +0200

    menuMobile/creating

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,11 +5,12 @@ import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { MenuMobileComponent } from '../shared/menu-mobile/menu-mobile.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
+  imports: [HomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent, FooterComponent, MenuMobileComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

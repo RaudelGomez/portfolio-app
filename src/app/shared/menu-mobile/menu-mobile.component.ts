d0b6f544a9f3a4bfacb55894d6e1fe6e9af0d3ab
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-07 11:58:51 +0200

    Translate/nav

--- src/app/shared/menu-mobile/menu-mobile.component.ts
+++ src/app/shared/menu-mobile/menu-mobile.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavService } from '../nav/nav.service';
+import { TranslationService } from '../nav/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-menu-mobile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './menu-mobile.component.html',
   styleUrl: './menu-mobile.component.scss'
 })
 export class MenuMobileComponent {
-  constructor(public navService:NavService){
-    console.log(navService.menu);
-  }
+  constructor(public navService:NavService, public translate: TranslationService){}
 }
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { NavService } from './nav.service';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from './translation.service';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-  constructor(public navService:NavService){}
+  constructor(public navService:NavService, public translate: TranslationService){}
 
 }

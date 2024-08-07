dad18b606d435ddfe6a45f1a33e27ac9a7d5cca3
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-07 19:10:25 +0200

    aboutMe/translated

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslationService } from '../../shared/nav/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+  constructor(private translate:TranslationService){}
 }
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslationService } from '../../shared/nav/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  constructor(private translate:TranslationService){}
 }

cd7f11775b9c930bc676ab0f954ab9b1d553f27e
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-08 13:25:30 +0200

    imprint/translated

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../shared/nav/translation.service';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+  constructor(private translate:TranslationService){}
 }
--- src/app/policy/policy.component.ts
+++ src/app/policy/policy.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../shared/nav/translation.service';
 
 @Component({
   selector: 'app-policy',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, TranslateModule],
   templateUrl: './policy.component.html',
   styleUrl: './policy.component.scss'
 })
 export class PolicyComponent {
-
+  constructor(private translate:TranslationService){}
 }

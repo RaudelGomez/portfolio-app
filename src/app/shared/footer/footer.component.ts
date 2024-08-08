29f0d4143fa0e6efbc7b1b12a4d710bc063e991e
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-08 12:48:44 +0200

    footer/translated

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslationService } from '../nav/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  constructor(private translate:TranslationService){}
 }

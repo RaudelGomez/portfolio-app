f096278fc061be0a358bd4cf51cd53bb5cba39eb
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-09 10:08:24 +0200

    menu/policy and imprint fixed

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../shared/nav/translation.service';
+import { MenuMobileComponent } from '../shared/menu-mobile/menu-mobile.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [FooterComponent, TranslateModule],
+  imports: [FooterComponent, TranslateModule, MenuMobileComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/policy/policy.component.ts
+++ src/app/policy/policy.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../shared/nav/translation.service';
+import { MenuMobileComponent } from '../shared/menu-mobile/menu-mobile.component';
 
 @Component({
   selector: 'app-policy',
   standalone: true,
-  imports: [FooterComponent, TranslateModule],
+  imports: [FooterComponent, TranslateModule, MenuMobileComponent],
   templateUrl: './policy.component.html',
   styleUrl: './policy.component.scss'
 })
